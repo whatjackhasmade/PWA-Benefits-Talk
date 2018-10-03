bc72b9e1c36aa2b77babc8939bb7b458b2811254
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2018-10-03 08:07:17 +0100

    Add slides

---
+++ theme.js
@@ -0,0 +1,13 @@
+// example theme.js
+import swiss from "mdx-deck/themes";
+
+export default {
+	// extends the default theme
+	...swiss,
+	// custom colors
+	colors: {
+		text: "#141213",
+		background: "#fff",
+		link: "#0652dd"
+	}
+};
